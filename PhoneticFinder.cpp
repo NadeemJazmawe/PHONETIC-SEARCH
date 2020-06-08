0dceee2e5cfd83647f79c3f719f8a84b9465113b
Nadeem Jazmawe <44744877+nadeemjazmawe@users.noreply.github.com>
2020-06-08 13:35:14 +0300

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -53,46 +53,36 @@ bool similar(char x,char y)
 
 }
 
-string find(string text,string word)
-{
-   string str = "";
-    int j = 0;
-    size_t i = 0;
-    for (i = 0; i < text.length(); i++)
-    {
+	string find (string x , string y){
+		string save = "";
+		int j = 0;
+		for(int i = 0 ; i < x.length() ; i++){
+			if (y == "")
+           		throw std::invalid_argument("Can't find the word in the text !");
+		
+           	while(x[i] == ' ')
+           		i++;
 
-        if (word == "")
-            throw std::invalid_argument("Can't find the word in the text !");
+           	while(y[j] == ' ')
+           		j++;
 
-        while (text[i] == ' ')
-        {
-            i++;
-        }
-        while (word[j]==' ')
-        {
-            j++;
-        }
+           	while((x.length() > i) && (y.length() > j) && (similar(y[i] , y[j])) && (x[i] != ' ')){
+           		save += x[i];
+           		j++;
+           		i++;
+           	}
 
-        while ((text.length() > i) && (word.length() > j) && (similar(text[i], word[j]))&&(text[i]!=' '))
-        {
-            str += text[i];
-            j++;
-            i++;
-        }
-        if (str.length() == word.length())
-            return str;
+           	if(y.length() == save.length())
+           		return save;
 
-        else
-        {
-            str.clear();
-            j = 0;
-        }
+           	else{
+           		j = 0;
+           		save.clear();
+           	}
 
-        if ((text.length() == i) && (word.length() != str.length()))
-        {
-            throw runtime_error("The word " + word + " is not in the text.");
-        }
-    }
-    throw runtime_error("The word " + word + " is not in the text.");    
-    }
+           	if((x.length() == i) && (y.length() != save.length()))
+           		throw runtime_error("The word " + y + " is not in the text.");
+		}
+		throw runtime_error("The word " + y + " is not in the text.");
+	}
 }
