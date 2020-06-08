7033867b5a53f196b53d8875ba209f2e368d9ef3
Nadeem Jazmawe <44744877+nadeemjazmawe@users.noreply.github.com>
2020-06-08 12:39:25 +0300

    Update PhoneticFinder.cpp

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -1,117 +1,125 @@
+Skip to content
+Search or jump to…
 
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@nadeemjazmawe 
+Code Issues 0 Pull requests 0 Projects 0 Actions Wiki Security 0 Pulse Community
+Phonetic-search-B/PhoneticFinder.cpp
+@zhra2zaid zhra2zaid Initial commit
+2570cca on Mar 30
+98 lines (68 sloc)  2.6 KB
+  
 #include <iostream>
-#include <string>
+#include <stdexcept>
+#include <string.h> 
 #include "PhoneticFinder.hpp"
 
 using namespace std;
 
-namespace phonetic {
 
-	bool help(char a , char b){
-		//if they similar
-		if(a == b)
-			return true;
-
-		//upper case
-		else if( (a==b+32) || (a=b-32))
-			return true;
-
-		//else all the other cases:-
-		else if(((a=='v')||(a=='V'))&&((b=='w')||(b=='W')))   
-			return true;     
-    	else if(((a=='w')||(a=='W'))&&((b=='v')||(b=='V')))   
-    		return true;
-
-    	else if((((a=='b')||(a=='B'))&&(((b=='f')||(b=='F'))||((b=='p')||(b=='P')))))  
-            return true;
-    	else if((((a=='f')||(a=='F'))&&(((b=='b')||(b=='B'))||((b=='p')||(b=='P')))))   
-    		return true;
-   		else if((((a=='p')||(a=='P'))&&(((b=='f')||(b=='F'))||((b=='b')||(b=='B')))))  
-   		    return true;
+namespace phonetic{
 
 
-   		else if(((a=='g')||(a=='G'))&&((b=='j')||(b=='J')))   
-   			return true;     
-    	else if(((a=='j')||(a=='J'))&&((b=='g')||(b=='G')))   
-    		return true;
+bool similar(char x,char y)
+{
+    if (x==y) return true;
+    if((x==y)||(x==y+32)||(x==y-32))    return true;
+       
+    if(((x=='v')||(x=='V'))&&((y=='w')||(y=='W')))   return true;     
+    if(((x=='w')||(x=='W'))&&((y=='v')||(y=='V')))   return true;
 
-    	else if((((a=='c')||(a=='C'))&&(((b=='k')||(b=='K'))||((b=='q')||(b=='Q')))))  
-    	 return true;
-    	else if((((a=='k')||(a=='K'))&&(((b=='c')||(b=='C'))||((b=='q')||(b=='Q')))))   
-    		return true;
-    	else if((((a=='q')||(a=='Q'))&&(((b=='c')||(b=='C'))||((b=='k')||(b=='K')))))   
-    		return true;
+    if((((x=='b')||(x=='B'))&&(((y=='f')||(y=='F'))||((y=='p')||(y=='P')))))   return true;
+    if((((x=='f')||(x=='F'))&&(((y=='b')||(y=='B'))||((y=='p')||(y=='P')))))   return true;
+    if((((x=='p')||(x=='P'))&&(((y=='f')||(y=='F'))||((y=='b')||(y=='B')))))   return true;
+
+
+    if(((x=='g')||(x=='G'))&&((y=='j')||(y=='J')))   return true;     
+    if(((x=='j')||(x=='J'))&&((y=='g')||(y=='G')))   return true;
+
+    if((((x=='c')||(x=='C'))&&(((y=='k')||(y=='K'))||((y=='q')||(y=='Q')))))   return true;
+    if((((x=='k')||(x=='K'))&&(((y=='c')||(y=='C'))||((y=='q')||(y=='Q')))))   return true;
+    if((((x=='q')||(x=='Q'))&&(((y=='c')||(y=='C'))||((y=='k')||(y=='K')))))   return true;
 
     
-    	else if(((a=='s')||(a=='S'))&&((b=='z')||(b=='Z')))   
-    		return true;     
-    	else if(((a=='Z')||(a=='z'))&&((b=='s')||(b=='S')))   
-    		return true;
+    if(((x=='s')||(x=='S'))&&((y=='z')||(y=='Z')))   return true;     
+    if(((x=='Z')||(x=='z'))&&((y=='s')||(y=='S')))   return true;
 
 
-    	else if(((a=='d')||(a=='D'))&&((b=='t')||(b=='T')))   
-    		return true;     
-    	else if(((a=='t')||(a=='T'))&&((b=='d')||(b=='D')))   
-    		return true;
+    if(((x=='d')||(x=='D'))&&((y=='t')||(y=='T')))   return true;     
+    if(((x=='t')||(x=='T'))&&((y=='d')||(y=='D')))   return true;
 
 
-    	else if(((a=='o')||(a=='O'))&&((b=='u')||(b=='U')))  
-    	 return true;     
-    	else if(((a=='u')||(a=='U'))&&((b=='o')||(b=='O')))   
-    		return true;
+    if(((x=='o')||(x=='O'))&&((y=='u')||(y=='U')))   return true;     
+    if(((x=='u')||(x=='U'))&&((y=='o')||(y=='O')))   return true;
 
 
-    	else if(((a=='i')||(a=='I'))&&((b=='y')||(b=='Y')))  
-    	 return true;     
-    	else if(((a=='y')||(a=='Y'))&&((b=='i')||(b=='I')))  
-    	 return true;  
+    if(((x=='i')||(x=='I'))&&((y=='y')||(y=='Y')))   return true;     
+    if(((x=='y')||(x=='Y'))&&((y=='i')||(y=='I')))   return true;  
 
 
-    	else
-    		return false;
-	}
+   
+            
+            return false;
 
 
-	/*
-	now i will write the main function that will find if there is similar word in the text.
-	a => the text
-	b => the word that we search
-	*/
- 	string find(string a , string b){
- 		if(a == "")
- 			return a;
- 		
- 		if ((b == "") || (a.length() < b.length()))
-            throw invalid_argument("there is no world similar to the word in the text !");
+}
 
+string find(string text,string word)
+{
+   string str = "";
+    int j = 0;
+    size_t i = 0;
+    for (i = 0; i < text.length(); i++)
+    {
 
- 		string s = "";
- 		size_t i = 0;
- 		int j = 0;
-
-        if (a == "" || b == "" )
+        if (word == "")
             throw std::invalid_argument("Can't find the word in the text !");
 
- 		for(i = 0 ; i < a.length() ; i++){
-            while (b[j]==' ')
-                j++;
-            while (a[i] == ' ')
-               i++;
+        while (text[i] == ' ')
+        {
+            i++;
+        }
+        while (word[j]==' ')
+        {
+            j++;
+        }
 
-            while ((help(a[i], b[j])) && (b.length() > i) && (a.length() > j)){
-                s += a[i];
-                j++;
-                i++;
-            }
+        while ((text.length() > i) && (word.length() > j) && (similar(text[i], word[j]))&&(text[i]!=' '))
+        {
+            str += text[i];
+            j++;
+            i++;
+        }
+        if (str.length() == word.length())
+            return str;
 
-            if (s.length() == b.length())
-                return s;
+        else
+        {
+            str.clear();
+            j = 0;
+        }
 
-            else{
-              s.clear();
-              j = 0;
-            }
- 		}
-		throw invalid_argument("there is no world similar to the word in the text !");
-	}
-};
+        if ((text.length() == i) && (word.length() != str.length()))
+        {
+            throw runtime_error("The word " + word + " is not in the text.");
+        }
+    }
+    throw runtime_error("The word " + word + " is not in the text.");    
+    }
+}
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
